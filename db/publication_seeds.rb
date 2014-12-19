092b071cde1ec1a8c4993d72f1388440ab60eb9b
Michael Trestman <michael.a.trestman@gmail.com>
2014-12-19 10:33:18 -0800

    updates seed file

--- db/publication_seeds.rb
+++ db/publication_seeds.rb
@@ -1,13 +1,234 @@
 trestman_to_z_pubs = [
+  {
+    title: "A Bat Without Qualities",
+    publication_date: "1993",
+    authors: ["Akins, K.A."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Review of Griffin 1992",
+    publication_date: "1992",
+    authors: ["Alcock, J."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Mental Content.",
+    publication_date: "1992",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Mental Content and Evolutionary Explanation.",
+    publication_date: "1992",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Intentionality: Natural and Artificial.",
+    publication_date: "1995",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Animal Cognition and Animal Minds.",
+    publication_date: "1997",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
 
   {
-    title: "",
-    publication_date: "",
-    authors: [],
+    title: "The Discovery of Animal Consciousness: An Optimistic Assessment.",
+    publication_date: "1997",
+    authors: ["Allen, C."],
     abstract: "",
-    link_urls: [],
-    proposed_origin_taxa: []
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
   },
+  {
+    title: "A skeptic's progress.",
+    publication_date: "2002",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Animal Pain.",
+    publication_date: "2004",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Is Anyone a Cognitive Ethologist?",
+    publication_date: "2004",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Fish cognition and consciousness.",
+    publication_date: "2013",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Species of Mind: The Philosophy and Biology of Cognitive Ethology.",
+    publication_date: "1997",
+    authors: ["Allen, C.", "Beckoff, M."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Deciphering Animal Pain.",
+    publication_date: "2005",
+    authors: ["Allen, C."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "The Lower Bounds of Cognition: What do spinal cords reveal?",
+    publication_date: "2009",
+    authors: ["Allen, C.", "Grau, J.", "Meagher, M."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "The Evolution of Reference",
+    publication_date: "1998",
+    authors: ["Allen, C.", "Saidel, E."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Doctor Dolittle's Delusion.",
+    publication_date: "2004",
+    authors: ["Anderson, S.R."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "The first step in the case for great ape equality: the argument for other minds.",
+    publication_date: "1996",
+    authors: ["Andrews, K."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Do Apes Read Minds? Toward a New Folk Psychology.",
+    publication_date: "2012",
+    authors: ["Andrews, K."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Evolution of Consciousness: Report of Agora Workshop in Sigtuna, Sweden, August 2001",
+    publication_date: "2002",
+    authors: ["Arhem, P.", "Liljenstrom, H.", "Lindahl, B.I.B."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "The Nature of Mind and Other Essays",
+    publication_date: "1980",
+    authors: ["Armstrong, D.A."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Affarent and efferent connections of the dorsolateral corticoid area in comparison with connections of the temporoparieto-occipital area in the pigeon (Columbia livia).",
+    publication_date: "2005",
+    authors: ["Atoji, Y.", "Wild, J.M."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "In the Theatre of Consciousness: Global Workspace Theory, A Rigorous Scientific Theory of Consciousness.",
+    publication_date: "1997",
+    authors: ["Baars, B.J."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Experiments and observations on the singing of birds.",
+    publication_date: "1773",
+    authors: ["Barrington, D."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   {
     title: "The Cambrian explosion and the origins of embodied cognition.",
@@ -45,6 +266,7 @@ trestman_to_z_pubs = [
     link_urls: [],
     proposed_origin_taxa: []
   },
+
   {
     title: "In Nature's Interests?",
     publication_date: "1998",
@@ -53,6 +275,7 @@ trestman_to_z_pubs = [
     link_urls: [],
     proposed_origin_taxa: []
   },
+
   {
     title: "Personhood, Ethics, and Animal Cognition: Situating Animals in Hare's Two Level Utilitarianism",
     publication_date: "2012",
@@ -95,52 +318,53 @@ trestman_to_z_pubs = [
     proposed_origin_taxa: []
   },
 
+  {
+    title: "The Natural History of Selbourne.",
+    publication_date: "1789",
+    authors: ["White, G."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+
+  {
+    title: "Cognitive abilities of araneophagic jumping spiders.",
+    publication_date: "1998",
+    authors: ["Wilcox, R.S.", "Jackson, R.R."],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Is consciousness important?",
+    publication_date: "1984",
+    authors: [Wilkes, K.],
+    abstract: "",
+    link_urls: [],
+    proposed_origin_taxa: []
+  },
+  {
+    title: "Animal ideas.",
+    publication_date: "1995",
+    authors: ["Wilson, M.D."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Do Animals Think?",
+    publication_date: "2004",
+    authors: ["Wynne, C."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  },
+  {
+    title: "Empathy and direct social perception.",
+    publication_date: "2011",
+    authors: ["Zahavi, D."],
+    abstract: "",
+    link_urls: [link],
+    proposed_origin_taxa: [proposed_origin_taxa]
+  }
 ]
-
-def log_pubs pubs
-  pubs.each do |pub|
-    if Publication.where({title: pub[:title]}).count > 0
-      db_pub = Publication.where({title: pub[:title]})[0]
-    else
-      db_pub = Publication.create({
-        title: pub[:title],
-        publication_date: pub[:publication_date]
-      })
-    end
-
-    pub[:authors].each do |auth|
-      this_author = {}
-      this_author[:name] = auth.split(',')
-      this_author[:last_name] = this_author[:name][0]
-      this_author[:first_name] = this_author[:name][1]
-
-      # db_auth = Researcher.where({
-      #   first_name: this_author.first_name,
-      #   last_name: this_author.last_name
-      # })[0]
-
-      if db_pub.authors.any?{ |db_auth|
-        this_author[:first_name] + this_author[:last_name] == db_auth.first_name + db_auth.last_name
-      }
-        {message: 'author previously in list'}
-      elsif Researcher.any?{ |db_auth|
-        this_author[:first_name] + this_author[:last_name]  == db_auth.first_name + db_auth.last_name
-      }
-        new_author = Researcher.where({
-          first_name: this_author[:first_name],
-          last_name: this_author[:last_name]
-        })[0]
-        db_pub.authors << new_author
-        new_author.publications << db_pub
-      else
-        new_author = Researcher.create({
-          first_name: this_author[:first_name],
-          last_name: this_author[:last_name]
-        })
-        db_pub.authors << new_author
-        new_author.publications << db_pub
-      end
-    end
-  end
-end
-log_pubs trestman_to_z_pubs
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,49 @@
 require_relative "publication_seeds"
 
+def log_pubs pubs
+  pubs.each do |pub|
+    if Publication.where({title: pub[:title]}).count > 0
+      db_pub = Publication.where({title: pub[:title]})[0]
+    else
+      db_pub = Publication.create({
+        title: pub[:title],
+        publication_date: pub[:publication_date]
+      })
+    end
+
+    pub[:authors].each do |auth|
+      this_author = {}
+      this_author[:name] = auth.split(',')
+      this_author[:last_name] = this_author[:name][0]
+      this_author[:first_name] = this_author[:name][1]
+
+      if db_pub.authors.any?{ |db_auth|
+        this_author[:first_name] + this_author[:last_name] == db_auth.first_name + db_auth.last_name
+      }
+        {message: 'author previously in list'}
+      elsif Researcher.any?{ |db_auth|
+        this_author[:first_name] + this_author[:last_name]  == db_auth.first_name + db_auth.last_name
+      }
+        new_author = Researcher.where({
+          first_name: this_author[:first_name],
+          last_name: this_author[:last_name]
+        })[0]
+        db_pub.authors << new_author
+        new_author.publications << db_pub
+      else
+        new_author = Researcher.create({
+          first_name: this_author[:first_name],
+          last_name: this_author[:last_name]
+        })
+        db_pub.authors << new_author
+        new_author.publications << db_pub
+      end
+    end
+  end
+end
+log_pubs trestman_to_z_pubs
+
+
 # animal_names = ['bears', 'elephants', 'unicrons', 'dinosaurs', 'velociraptors', 't-rexes', 'whales', 'dolphins', 'platypus', 'octopus', 'squids', 'shrimps', 'wormy things', 'clams' ]
 # animal_names.each { |a| Taxon.create({name: a}) }
 
